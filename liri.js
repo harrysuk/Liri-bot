81676d7b26868e6287390ad4729a5259c016566a
harry suk <harrydialsukhram@gmail.com>
2018-12-10 18:14:59 -0500

    file update/error fixes

---
+++ liri.js
@@ -0,0 +1,106 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var moment = require("moment");
+var axios = require("axios");
+var fs = require("fs");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var divider = "\n------------------------------------------------\n";
+
+var search = process.argv[2];
+var term = process.argv.slice(3).join(" ");
+
+function liriBot() {
+    switch (command) {
+      case "concert-this":
+        console.log("Searching for Your Concert \n----------------------");
+        concertThis();
+        break;
+      case "spotify-this":
+        console.log("Searching for Your Song \n----------------------");
+        spotifyThis();
+        break;
+      case "movie-this":
+      console.log("Searching for Your Movie \n----------------------");        
+        movieThis();
+        break;
+      case "do-what-it-says":
+      console.log("Searching for Your Stuff \n----------------------");
+        fileInfo();
+        break;
+      default:
+        console.log("invalid command please try again");
+    }
+  }
+
+
+//   concert search
+
+function concertThis() {
+    var artist = term
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+    axios
+        .get(queryURL)
+        .then(function(response){
+            var concertData = response.data
+            for (var i = 0; i < response.data.length; i++) {
+                console.log("Venue name: " + concertInfo[i].venue.name)
+                console.log("City: " + concertInfo[i].venue.city)
+                console.log("Concert Date: " + moment(concertInfo[i].datetime).format("MM/DD/YYYY"))
+        } })
+        fs.appendFile("log.txt", logCommand + concertData + divider, function(err) {
+            if (err) throw err;
+          });
+
+        };
+
+// Spotify search
+function spotifyThis() {
+    var tracks = term;
+
+    spotify
+        .search({ type: 'track', query: tracks })
+        .then(function(response) {
+        console.log(key("\nArtist: ") + response.tracks.items[0].artists[0].name);
+        console.log(key("Song: ") + response.tracks.items[0].name);
+        console.log(key("Preview: ") + response.tracks.items[0].preview_url);
+        console.log(key("Album Title: ") + response.tracks.items[0].album.name);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+
+        fs.appendFile("log.txt", "\n" +  search + "\n" + song + divider, function(err) {
+            if (err) throw err;
+         });
+        };
+
+// Do whatever
+
+function fileInfo(){
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+          }
+          var filedata = data.split(",")
+          var  dataArr = filedata[1].replace(/"/g,'');
+  
+
+      if (filedata[0] === "spotify-this") {
+        spotifyThis(song = grabName);
+
+      } else if (filedata[0] === "movie-this") {
+        movieThis(movie = grabName);
+
+      } else if (filedata[0] === "concert-this") {
+        concertThis(artist = grabName);
+      }; 
+
+      if (error) {
+        return console.log(error);
+      }; 
+
+}
+
